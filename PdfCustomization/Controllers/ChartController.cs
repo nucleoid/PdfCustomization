135cdbee4dff709d841344764a9c4575502db627
Mitch Statz <mstatz@mail.com>
2011-10-20 09:50:20 -0600

    Finished up chart spike

--- PdfCustomization/Controllers/ChartController.cs
+++ PdfCustomization/Controllers/ChartController.cs
@@ -25,22 +25,89 @@ namespace PdfCustomization.Controllers
                 new Price {Mode = Mode.Online, UnitPrice = 57.89m, TotalPrice = 926.24m}
             };
 
-            var chart = new Chart { BackColor = Color.Transparent, Width = Unit.Pixel(800), Height = Unit.Pixel(400)};
+            var font = new Font("Trebuchet MS", 8.25f, FontStyle.Bold);
+            var chart = new Chart
+                            {
+                                Palette = ChartColorPalette.BrightPastel, 
+                                BackColor = Color.DarkOrange, 
+                                ImageType = ChartImageType.Png,
+                                Width = Unit.Pixel(824),
+                                Height = Unit.Pixel(592),
+                                BorderlineDashStyle = ChartDashStyle.Solid,
+                                BackGradientStyle = GradientStyle.TopBottom,
+                                BorderWidth = 2,
+                                BorderColor = Color.FromArgb(181, 64, 1),
+                            };
+            chart.Legends.Add("Legend1");
+            chart.Legends[0].IsTextAutoFit = false;
+            chart.Legends[0].BackColor = Color.Transparent;
+            chart.Legends[0].Font = font;
+            chart.BorderSkin.SkinStyle = BorderSkinStyle.Emboss;
 
-            var ca1 = new ChartArea("ca1") {BackColor = Color.Transparent, AxisX = {Title = "Entry"}, AxisY = {Title = "Price"}};
+            var ca1 = new ChartArea("ca1")
+                          {
+                              BorderColor = Color.FromArgb(64),
+                              BorderDashStyle = ChartDashStyle.Solid,
+                              BackSecondaryColor = Color.White,
+                              BackColor = Color.OldLace,
+                              ShadowColor = Color.Transparent,
+                              BackGradientStyle = GradientStyle.TopBottom,
+                              AxisX =
+                                  {
+                                      Title = "Entry", 
+                                      LineColor = Color.FromArgb(64),
+                                      LabelStyle = new LabelStyle {Font = font},
+                                      MajorGrid = new Grid {LineColor = Color.FromArgb(64), Enabled = true}
+                                  },
+                              AxisY =
+                                  {
+                                      Title = "Price",
+                                      LineColor = Color.FromArgb(64),
+                                      LabelStyle = new LabelStyle { Font = font },
+                                      MajorGrid = new Grid { LineColor = Color.FromArgb(64), Enabled = true}
+                                  }, 
+                          };
             chart.ChartAreas.Add(ca1);
 
-            var font = new Font("Trebuchet MS", 8.25f);
-            var series1 = new Series("Unit Price") { ChartType = SeriesChartType.Line, ChartArea = "ca1", IsValueShownAsLabel = true, Font = font };
-            var series2 = new Series("Total Price") { ChartType = SeriesChartType.Line, ChartArea = "ca1", IsValueShownAsLabel = true, Font = font };
-            foreach (var unitPrice in prices.Select(x => x.UnitPrice))
+            var series1 = new Series("Unit Price")
+                              {
+                                  MarkerSize = 8,
+                                  BorderWidth = 3,
+                                  XValueType = ChartValueType.Double,
+                                  ChartType = SeriesChartType.Line, 
+                                  MarkerStyle = MarkerStyle.Circle,
+                                  ShadowColor = Color.Black,
+                                  BorderColor = Color.Black,
+                                  Color = Color.DarkTurquoise,
+                                  ShadowOffset = 2,
+                                  YValueType = ChartValueType.Double,
+                                  ChartArea = "ca1", 
+                                  IsValueShownAsLabel = false, 
+                                  Font = font
+                              };
+            var series2 = new Series("Total Price")
+                              {
+                                  MarkerSize = 9,
+                                  BorderWidth = 3,
+                                  XValueType = ChartValueType.Double,
+                                  ChartType = SeriesChartType.Line,
+                                  MarkerStyle = MarkerStyle.Diamond,
+                                  ShadowColor = Color.Black,
+                                  BorderColor = Color.Black,
+                                  Color = Color.Red,
+                                  ShadowOffset = 2,
+                                  YValueType = ChartValueType.Double,
+                                  ChartArea = "ca1",
+                                  IsValueShownAsLabel = false,
+                                  Font = font
+                              };
+            foreach (var unitPrice in prices.Select(x => Convert.ToDouble(x.UnitPrice)))
                 series1.Points.AddY(unitPrice);
-            foreach (var totalPrice in prices.Select(x => x.TotalPrice))
+            foreach (var totalPrice in prices.Select(x => Convert.ToDouble(x.TotalPrice)))
                 series2.Points.AddY(totalPrice);
             chart.Series.Add(series1);
             chart.Series.Add(series2);
-            chart.Legends.Add("Legend1");
-
+            
             using (var ms = new MemoryStream())
             {
                 chart.SaveImage(ms, ChartImageFormat.Png);
